a2756994a8fb6ef38b050da17a61d38bf55aaef1
meannnph <122788933+meannnph@users.noreply.github.com>
2023-08-03 08:40:19 +0700

    Update main.js

--- web-ifc-viewer/example/main.js
+++ web-ifc-viewer/example/main.js
@@ -229,3 +229,62 @@ dropBoxButton.addEventListener('click', () => {
   dropBoxButton.blur();
   viewer.dropbox.loadDropboxIfc();
 });
+
+// Properties menu
+
+window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
+
+window.ondblclick = async () => {
+    const result = await viewer.IFC.selector.highlightIfcItem();
+    if (!result) return;
+    const { modelID, id } = result;
+    const props = await viewer.IFC.getProperties(modelID, id, true, false);
+    createPropertiesMenu(props);
+};
+
+const propsGUI = document.getElementById("ifc-property-menu-root");
+
+function createPropertiesMenu(properties) {
+    console.log(properties);
+
+    removeAllChildren(propsGUI);
+
+    const psets = properties.psets;
+    const mats = properties.mats;
+    const type = properties.type;
+
+    delete properties.psets;
+    delete properties.mats;
+    delete properties.type;
+
+
+    for (let key in properties) {
+        createPropertyEntry(key, properties[key]);
+    }
+
+}
+
+function createPropertyEntry(key, value) {
+    const propContainer = document.createElement("div");
+    propContainer.classList.add("ifc-property-item");
+
+    if(value === null || value === undefined) value = "undefined";
+    else if(value.value) value = value.value;
+
+    const keyElement = document.createElement("div");
+    keyElement.textContent = key;
+    propContainer.appendChild(keyElement);
+
+    const valueElement = document.createElement("div");
+    valueElement.classList.add("ifc-property-value");
+    valueElement.textContent = value;
+    propContainer.appendChild(valueElement);
+
+    propsGUI.appendChild(propContainer);
+}
+
+function removeAllChildren(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
